6d0dd69853920caf01c97f0c9fcb570443a2f1e4
Lucas Piorno <lucasp01012002@gmail.com>
2023-12-07 13:01:03 +0200

    phonebook 3.22

--- index.js
+++ index.js
@@ -1,137 +1,142 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const PORT = process.env.PORT
 
 const database = require('./database')
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
 app.use(express.static('dist'))
-app.use(cors());
-app.use(express.json());
-app.use(morgan('tiny'));
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`)
 })
 
 app.get('/api/persons', (request, response) => {
-    database.fetchAllContacts()
-        .then(contacts => {
-            response.json(contacts)
-        })
-        .catch(error => {
-            console.log(error)
-            response.status(404).end()
-        })
+	database.fetchAllContacts()
+		.then(contacts => {
+			response.json(contacts)
+		})
+		.catch(error => {
+			console.log(error)
+			response.status(404).end()
+		})
 }) 
 
 // Get contact by id
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    console.log(id)
-    database.fetchContactById(id)
-        .then(contact => {
-            // If the contact exists, return it, else return 404
-            if (contact) {
-                response.json(contact)
-            }
-            response.status(404).end()
-        })
-        .catch(error => {
-            next(error)
-        })
+	const id = request.params.id
+	console.log(id)
+	database.fetchContactById(id)
+		.then(contact => {
+			// If the contact exists, return it, else return 404
+			if (contact) {
+				response.json(contact)
+			}
+			response.status(404).end()
+		})
+		.catch(error => {
+			next(error)
+		})
 })
 
 // Add contact
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    const name = body.name
-    const number = body.number
+	const body = request.body
+	const name = body.name
+	const number = body.number
 
-    const contact = {
-        name: name,
-        number: number,
-    }
+	const contact = {
+		name: name,
+		number: number,
+	}
 
-    // Add contact to database
-    database.addContact(contact)
-        .then(contact => {
-            response.json(contact)
-        })
-        .catch(error => {
-            next(error)
-        })
+	// Add contact to database
+	database.addContact(contact)
+		.then(contact => {
+			response.json(contact)
+		})
+		.catch(error => {
+			next(error)
+		})
 })
 
 // Delete contact by id
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
+	const id = request.params.id
 
-    // Delete contact from database
-    database.deleteContact(id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => {
-            next(error)
-        })
+	// Delete contact from database
+	database.deleteContact(id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => {
+			next(error)
+		})
 })
 
 // Update contact
 app.put('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    const body = request.body
-    const name = body.name
-    const number = body.number
+	const id = request.params.id
+	const body = request.body
+	const name = body.name
+	const number = body.number
 
-    // Check parameters
-    if (!name) {
-        return response.status(400).json({ 
-            error: 'name missing' 
-        })
-    }
+	// Check parameters
+	if (!name) {
+		return response.status(400).json({ 
+			error: 'name missing' 
+		})
+	}
 
-    if (!number) {
-        return response.status(400).json({ 
-            error: 'number missing' 
-        })
-    }
+	if (!number) {
+		return response.status(400).json({ 
+			error: 'number missing' 
+		})
+	}
 
-    // Update contact in database
-    database.updateContact(id, name, number)
-        .then(() => {
-            response.status(200).end()
-        })
-        .catch(error => {
-            next(error)
-        })
+	// Update contact in database
+	database.updateContact(id, name, number)
+		.then(() => {
+			response.status(200).end()
+		})
+		.catch(error => {
+			next(error)
+		})
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${contacts.length} people</p>
-    <p>${new Date()}</p>`)
+	database.fetchAllContacts()
+		.then(contacts => {
+			response.send(`<p>Phonebook has info for ${contacts.length} people</p>
+            <p>${new Date()}</p>`)
+		})
+		.catch(error => {
+			console.log(error)
+			response.status(404).end()
+		})
 })
 
 // Error handling
-const errorHandler = (error, request, response, next) => {
-    console.error("uauauau", error.message)
+const errorHandler = (error, request, response, next) => {    
+	if (error.name === 'CastError') {
+		return response.status(400).send({ 
+			error: 'malformatted id' 
+		})
+	}
+	else if (error.name === 'ValidationError') {
+		return response.status(400).json({ 
+			error: error.message 
+		})
+	}
     
-    if (error.name === 'CastError') {
-        return response.status(400).send({ 
-            error: 'malformatted id' 
-        })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ 
-            error: error.message 
-        })
-    }
-    
-    next(error)
+	next(error)
 }
 
 app.use(errorHandler)
